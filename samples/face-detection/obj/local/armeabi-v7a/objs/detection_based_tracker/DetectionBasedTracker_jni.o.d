ec8ed8a841da67fe2fe216ae8545361c8c991057
Ishan Saksena <ishansaksena@gmail.com>
2014-10-18 00:48:01 -0700

    initial commit

---
+++ samples/face-detection/obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,145 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/jni.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ ../../sdk/native/jni/include/opencv2/core/core.hpp \
+ ../../sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/string.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/math.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ ../../sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ ../../sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ ../../sdk/native/jni/include/opencv2/core/mat.hpp \
+ ../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp \
+ ../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/log.h
+
+jni/DetectionBasedTracker_jni.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+../../sdk/native/jni/include/opencv2/core/core.hpp:
+
+../../sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+../../sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+../../sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+../../sdk/native/jni/include/opencv2/core/mat.hpp:
+
+../../sdk/native/jni/include/opencv2/contrib/detection_based_tracker.hpp:
+
+../../sdk/native/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Users/isaks/android-ndk-r10c/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/isaks/android-ndk-r10c/platforms/android-8/arch-arm/usr/include/android/log.h:
---
+++ samples/native-activity/obj/local/armeabi-v7a/objs-debug/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,200 @@
+obj/local/armeabi-v7a/objs-debug/android_native_app_glue/android_native_app_glue.o: \
+ /Users/isaks/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.c \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/errno.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/resource.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/resource.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/resource.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h \
+ /Users/isaks/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ /Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/resource.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/resource.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/resource.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h:
+
+/Users/isaks/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+/Users/isaks/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h:
